19292ed2cb5224b7762a983350fabcbd64434d26
JaviARo <95490801+JaviARo@users.noreply.github.com>
2022-09-22 20:18:49 +0100

    Update AppUserController.java

--- backend/src/main/java/com/putacollartoyourchild/app/controllers/AppUserController.java
+++ backend/src/main/java/com/putacollartoyourchild/app/controllers/AppUserController.java
@@ -2,8 +2,11 @@ package com.putacollartoyourchild.app.controllers;
 
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,4 +30,10 @@ public class AppUserController {
 	public void insert(AppUser appUser) {
 		appUserService.insert(appUser);
 	}
+	
+	@DeleteMapping("/appuser/{id}")
+	public void delete(@PathParam(value = "id") long id) {
+		
+	}
+	
 }
